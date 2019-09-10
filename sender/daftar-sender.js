34c4533b1fd75cbce20c9baa4a0b2ad72cead500
Ade Hikmat <aalolxyz@gmail.com>
2019-09-10 23:12:32 +0700

    regal galing

---
+++ sender/daftar-sender.js
@@ -0,0 +1,8 @@
+const Daftar=require('../models/anggota');
+
+const daftar = [
+   new Daftar({nama : 'Regal',
+               alamat : 'Ununen'}),
+   new Daftar({nama : 'Fikri', alamat : 'kawal'})
+
+];
